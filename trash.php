ff904a3edb0f12a7b071b655c3ce529eaa1b0b39
iuliangeorgepetre <50615636+iuliangeorgepetre@users.noreply.github.com>
2019-05-15 19:40:45 +0300

    Add files via upload

--- trash.php
+++ trash.php
@@ -1,13 +1,19 @@
 <?php
 include "header.php";
 require "includes/dbh.inc.php";
-
+$este = 0;
+$uid = $_SESSION['id'];
+$rez = $conn->query("SELECT * FROM 'user_task_leg' WHERE idUsers = $uid ");
+$taskId = $rez['idTask'];
 $query = $conn->query("SELECT * FROM `tasks` WHERE status = 'inTrash' ");
+
+
 
 
 if (!$query->fetch_array()) {
     echo 'No items in the trash';
 } else {
+
     if (isset($_POST['save'])) {
         $checkbox = $_POST['check'];
         for ($i = 0; $i < count($checkbox); $i++) {
@@ -16,7 +22,12 @@ if (!$query->fetch_array()) {
             $message = "Data deleted successfully !";
         }
     }
-    $result = mysqli_query($conn, "SELECT * FROM tasks");
+
+
+
+
+
+    $result = mysqli_query($conn, "SELECT * FROM tasks WHERE status = 'inTrash'");
     ?>
     <br>
     <div class="col-md-3"></div>
@@ -34,40 +45,58 @@ if (!$query->fetch_array()) {
                             <th>Task</th>
                         </tr>
                     </thead>
-                    <?php
-                    $i = 1;
-                    while ($row = mysqli_fetch_array($result)) {
-                        ?>
-                        <tr>
-                            <td><input type="checkbox" id="checkItem" name="check[]" value="<?php echo $row["idTask"]; ?>"></td>
-                            <td><?php echo $i; ?></td>
-                            <td><?php echo $row["name"]; ?></td>
-                            <td>
-                                <a href="deleteonlyone.php?id=<?php echo $row["idTask"]; ?>"><span class="glyphicon glyphicon-trash"></span></a>
-
-
-                            </td>
-
-                        </tr>
+                    <br><br><br>
+                    <tbody class="text-center">
                         <?php
-                        $i++;
+                        require "includes/dbh.inc.php";
+
+                        $id = $_SESSION['id'];
+
+                        $interogare = $conn->query("SELECT * FROM `user_task_leg` WHERE idUsers = $id ");
+
+
+                        $count = 1;
+                        while ($iadate = $interogare->fetch_array()) {
+                            $idTask = $iadate['idTask'];
+
+                            $query = $conn->query("SELECT * FROM `tasks` WHERE idTask = $idTask ");
+
+
+
+                            while ($fetch = $query->fetch_array()) {
+                                $este = 1;
+                                ?>
+                                <tr>
+                                    <td><input type="checkbox" id="checkItem" name="check[]" value="<?php echo $fetch["idTask"]; ?>"></td>
+                                    <td><?php echo $count++ ?></td>
+                                    <td><?php echo $fetch['name'] ?></td>
+                                    <td><a href="deleteonlyone.php?id=<?php echo $fetch["idTask"]; ?>"><span class="glyphicon glyphicon-trash"></span></a></td>
+
+
+                                </tr>
+
+                            <?php
+                        }
                     }
                 }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+                if ($este)
+                    echo '<button type="submit" class="btn btn-danger" name="save">Permanently delete<span class="glyphicon glyphicon-trash"></span></button>';
                 ?>
-            </table>
-            <?php
-            if ($query->fetch_array())
-                echo '<button type="submit" class="btn btn-danger" name="save">Permanently delete<span class="glyphicon glyphicon-trash"></span></button>';
-            ?>
 
-        </form>
+            </form>
+
+
+        </div>
     </div>
-</div>
 
 
 
-<script>
-    $("#checkAl").click(function() {
-        $('input:checkbox').not(this).prop('checked', this.checked);
-    });
-</script>
+    <script>
+        $("#checkAl").click(function() {
+            $('input:checkbox').not(this).prop('checked', this.checked);
+        });
+    </script>
