aa9d7e074e5ca15f3dcf33a4ddd1cf037b6cfc93
AlexandraChiha <50616078+AlexandraChiha@users.noreply.github.com>
2019-05-18 23:50:09 +0300

    Update todolist.php

--- todolist.php
+++ todolist.php
@@ -1,15 +1,6 @@
-
 <head>
-
-
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-
-
-
 </head>
-
 <body>
     <div class="col-md-3"></div>
     <div class="col-md-6 well">
@@ -17,15 +8,12 @@
         <hr style="border-top:1px dotted #ccc;" />
         <div class="col-md-2"></div>
         <div class="col-md-8 center">
-
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-                Add Task
+            
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+            Add Task
             </button>
-
-
             <!-- The Modal Task -->
             <form method="POST" class="form" action="add_query.php">
-
                 <div class="modal" id="myModal">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -44,9 +32,7 @@
                                 <br>
                                 <label for="deadline">Deadline</label>
                                 <input type="date" class="form-control" name="deadline" required>
-
                                 <input type="hidden" class="form-control" name="bookId" value="0">
-
                                 <br>
                                 <button class="btn btn-primary form-control" name="add">Add Task</button>
                                 <br><br>
@@ -59,7 +45,6 @@
                     </div>
                 </div>
             </form>
-
             <!-- The Modal Subtask -->
             <form method="POST" class="form" action="add_query.php">
                 <div class="modal" id="myModalSubtask">
@@ -93,17 +78,16 @@
                     </div>
                 </div>
             </form>
-
             <!-- Modal Edit -->
- 
+            
             <form method="POST" class="form" action="edit_task.php">
                 <div class="modal" id="myModalEdit">
-                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <div class="modal-dialog">
-                            <div class="modal-content">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
                             <!-- Modal Header -->
                             <div class="modal-header">
-                                <h4 class="modal-title">Edit Task</h4>                           
+                                <h4 class="modal-title">Edit Task</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
                             <!-- Modal body -->
                             <div class="modal-body">
@@ -114,9 +98,9 @@
                                 <input type="date" class="form-control" name="deadline" required>
                                 <br>
                                 <input type="hidden" class="form-control" name="bookId" value="">
-
                                 <button class="btn btn-primary form-control" name="edit">EditTask</button>
                                 <br><br>
+                                
                             </div>
                             <!-- Modal footer -->
                             <div class="modal-footer">
@@ -126,157 +110,184 @@
                     </div>
                 </div>
             </form>
-
         </div>
-        <div class="col-md-2"></div>
         <br /><br /><br />
-        <table class="table table-hover">
-            <thead class="text-center">
-                <tr class="text-center">
-                    <th scope="col">#</th>
-                    <th scope="col">Task</th>
-                    <th scope="col">Deadline</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="col-md-8">
+                        <h4>
+                        Task
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        require "includes/dbh.inc.php";
+        $id = $_SESSION['id'];
+        $interogare = $conn->query("SELECT * FROM `user_task_leg` WHERE idUsers = $id ");
+        $count = 1;
+        while ($iadate = $interogare->fetch_array()) {
+        $idTask = $iadate['idTask'];
+        $query = $conn->query("SELECT * FROM `tasks` WHERE idTask = $idTask AND status !='inTrash' AND parent_ID = '0'");
+        while ($fetch = $query->fetch_array()) {
+        # if($fetch[])
+        ?>
+        <div class="row">
+            <div class="col-md-1">
                 <?php
-                require "includes/dbh.inc.php";
-
-                $are=0;
-
-                $id = $_SESSION['id'];
-
-                $interogare = $conn->query("SELECT * FROM `user_task_leg` WHERE idUsers = $id ");
-
-                $count = 1;
-                while ($iadate = $interogare->fetch_array()) {
-                    $idTask = $iadate['idTask'];
-
-
-                    $query = $conn->query("SELECT * FROM `tasks` WHERE idTask = $idTask AND status !='inTrash' AND parent_ID = '0'");
-
-                    while ($fetch = $query->fetch_array()) {
-                        # if($fetch[])
-                        ?>
-                        <tr class="text-center">
-
-                            <td><?php echo $count++ ?></td>
-                            <td><?php echo $fetch['name'] ?></td>
-                            <td><?php echo $fetch['deadline'] ?></td>
-                            <td><?php echo $fetch['status'] ?></td>
-                            <td colspan="3">
-                                <?php
-                                if ($fetch['status'] == "Undone") {
-                                    echo
-                                        '<a href="update_task.php?task_id=' . $fetch['idTask'] . '" class="btn btn-success" title="Complete Task"><span class="glyphicon glyphicon-check"></span></a> |';
-                                }
-                                if ($fetch['status'] == "Done") {
-                                    echo
-                                        '<a href="update_task2.php?task_id=' . $fetch['idTask'] . '" class="btn btn-primary" title="Restore Task"><span class="glyphicon 
-                                        glyphicon-refresh"></span></a> |';
-                                }
-                                ?>
-                                <a href="ToTrash.php?task_id=<?php echo $fetch['idTask'] ?>" class="btn btn-danger" title="Delete Task"><span class="glyphicon glyphicon-remove"></span></a><span>|</span>
-
-                                <button class="btn btn-primary" data-toggle="collapse"  title="About..." data-target="#collapseExample<?php echo $count - 1 ?>" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    <span class="glyphicon glyphicon-question-sign"></span>
-                                </button><span>|</span>
-                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModalSubtask" data-book-id="<?php echo $fetch['idTask'] ?> " title="Add Subtask">
-                                    <span class="glyphicon glyphicon-plus-sign"></span>
-
-                              </button><span>|</span>
-                                 <button type-="button" class="btn btn-info"data-toggle="modal" data-target="#myModalEdit" data-book-id="<?php echo $fetch['idTask'] ?> " title="Edit Task"><span class="glyphicon glyphicon-edit"></span></button>
-                                
-                            </td>
-                            <td>
-                                <div class="collapse" id="collapseExample<?php echo $count - 1 ?>">
-                                    <div class="text-center">
-                                        <?php echo $fetch['descriere']; ?>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                        <?php
-                            $query = $conn->query("SELECT * FROM `tasks` WHERE parent_ID = $idTask");
-                            if($fetch = $query->fetch_array()){
-                                $are=1;
-                                echo'
-                                <div>
-                               
-                                
-                                ';
-                            }
-                            $query = $conn->query("SELECT * FROM `tasks` WHERE parent_ID = $idTask");
-                            while($fetch = $query->fetch_array()){
-                        ?>
-                        
-                        <tr class="text-center">
-                            
-                            <td> </td>
-                            <td style = "font-weight:bold; background-color:#EA2027; color:white;"><?php echo $fetch['name'] ?></td>
-                            <td style = "font-weight:bold; background-color:#EA2027; color:white;"><?php echo $fetch['deadline'] ?></td>
-                            <td style = "font-weight:bold; background-color:#EA2027; color:white;"><?php echo $fetch['status'] ?></td>
-                            <td>
-                            <?php
-                                if ($fetch['status'] == "Undone") {
-                                    echo
-                                        '<a href="update_task.php?task_id=' . $fetch['idTask'] . '" class="btn btn-success" title="Complete Task"><span class="glyphicon glyphicon-check"></span></a> ';
-                                }
-                                if ($fetch['status'] == "Done") {
-                                    echo
-                                        '<a href="update_task2.php?task_id=' . $fetch['idTask'] . '" class="btn btn-primary" title="Restore Task"><span class="glyphicon 
-                                        glyphicon-refresh"></span></a> ';
-                                }
-                                ?>
-                            </td>
-
-                        </tr>
-                    <?php
-                         }
-                         echo "</div>";
-                        
-                         
-
+                if ($fetch['status'] == "Undone") {
+                echo
+                '<a href="update_task.php?task_id=' . $fetch['idTask'] . '" class="btn btnoutline-success" title="Complete Task"><span class="glyphicon glyphicon-ok"></span></a> ';
                 }
-            }
-            ?>
-            </tbody>
-        </table>
-    </div>
-</body>
-<script>
+                if ($fetch['status'] == "Done") {
+                echo
+                '<a href="update_task2.php?task_id=' . $fetch['idTask'] . '" class="btn btn-outline-primary" title="Restore Task"><span class="glyphicon
+                glyphicon-refresh"></span></a> ';
+                }
+                ?>
+            </div>
+            <div class="col-md-8">
+                <button id="wrap"class="accordion"><?php echo $fetch['name'] ?></button>
+                <div class="panel">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Description</th>
+                                <th scope="col">Deadline</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> <?php echo $fetch['descriere']; ?></td>
+                                <td> <?php echo $fetch['deadline'] ?></td>
+                                <td><?php echo $fetch['status'] ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                    $query = $conn->query("SELECT * FROM `tasks` WHERE parent_ID = $idTask");
+                    while($fetch2 = $query->fetch_array()){
+                    ?>
+                    <div class="row">
+                        
+                        <div class="col-md-12">
+                            
+                            <table class="table">
+                                <thead>
+                                    <tr style="background-color: lightgrey;">
+                                        <th></th>
+                                        <th scope="col">Subtask</th>
+                                        <th scope="col">Deadline</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <?php
+                                            if ($fetch2['status'] == "Undone") {
+                                            echo
+                                            '<a href="update_task.php?task_id=' . $fetch2['idTask'] . '" class="btn btnoutline-success" title="Complete Task"><span class="glyphicon glyphicon-ok"></span></a> ';
+                                            }
+                                            if ($fetch2['status'] == "Done") {
+                                            echo
+                                            '<a href="update_task2.php?task_id=' . $fetch2['idTask'] . '" class="btn btn-outline-primary" title="Restore Task"><span class="glyphicon
+                                            glyphicon-refresh"></span></a> ';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td> <?php echo $fetch2['name']; ?></td>
+                                        <td> <?php echo $fetch2['deadline']; ?></td>
+                                        <td><?php echo $fetch2['status']; ?></td>
+                                        <td>
+                                            
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="ToTrash.php?task_id=<?php echo $fetch2['idTask']; ?>" class="btn btn-outline-danger"><span class="glyphicon glyphicon-remove"></span></a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a type-="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#myModalEdit" data-book-id="<?php echo $fetch2['idTask']; ?> "><span class="glyphicon glyphicon-edit"></span></a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+            
+            <div class="col-md-1">
+                <a href="ToTrash.php?task_id=<?php echo $fetch['idTask']?>" class="btn btn-outline-danger"><span class="glyphicon glyphicon-remove"></span></a>
+            </div>
+            <div class="col-md-2">
+                <div class="dropdown">
+                    <div class="btn-group dropright">
+                        <button class="btn btn-primary dropdown-toggle " type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-option-vertical"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a type="button" class="dropdown-item" data-toggle="modal" data-target="#myModalSubtask" data-book-id="<?php echo $fetch['idTask'] ?> "><span class="glyphicon glyphicon-plus-sign"></span> Add SubTask</a></li>
+                            <li><a type-="button" class="dropdown-item" data-toggle="modal" data-target="#myModalEdit" data-book-id="<?php echo $fetch['idTask'] ?> "><span class="glyphicon glyphicon-edit"></span> Edit</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php }
+        }
+        ?>
+        
+        
+    </body>
+    <script>
     // When the user clicks on <div>, open the popup
-    $('#myModal').modal(options);
-
-    function myFunction() {
+        $('#myModal').modal(options);
+        function myFunction() {
         var popup = document.getElementById("myPopup");
         popup.classList.toggle("show");
-    }
-</script>
-<script>
-
-    $('.collapse').collapse({
-        toggle: false
-    });
-    $('#myModalSubtask').on('show.bs.modal', function(e) {
+        }
+        </script>
+        <script>
+        $('#myModalSubtask').on('show.bs.modal', function(e) {
         //get data-id attribute of the clicked element
         var bookId = $(e.relatedTarget).data('book-id');
         //populate the textbox
         $(e.currentTarget).find('input[name="bookId"]').val(bookId);
-    });
-    $('#myModalEdit').on('show.bs.modal', function(e) {
+        });
+        $('#myModalEdit').on('show.bs.modal', function(e) {
         //get data-id attribute of the clicked element
         var bookId = $(e.relatedTarget).data('book-id');
-
         //populate the textbox
         $(e.currentTarget).find('input[name="bookId"]').val(bookId);
-    });
-    $(function () {
+        });
+        $(function () {
         $('[data-toggle="tooltip"]').tooltip();
-    });
-
-</script>
+        });
+        </script>
+        <script>
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+        for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        }
+        });
+        }
+        </script>
+        <script>
+        function changecolor() {
+        document.getElementById('wrap').style.backgroundColor = "green";
+        }
+        </script>
